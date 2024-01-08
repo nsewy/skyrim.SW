1e21b537dc7885f96ac8b6e7ed6f1a2d791efc76
nsewy <149711531+nsewy@users.noreply.github.com>
2024-01-07 22:48:23 -0800

    Add files via upload

---
+++ skyrim.bat
@@ -0,0 +1,27 @@
+k > 112
+k.weedyh
+color 06
+echo on
+/POTATO '1A'prompt code = player.additem 00064B41' ;
+echo off
+@pause
+w > 111
+k.weedyg
+color 04
+echo on
+/TOMATO 1B'prompt code = player.additem 00064B42' ; 
+echo off
+@pause
+x > 110 < 1C
+k.weedyx
+color 06
+echo on
+/WHEEL '1C'prompt code = player.additem 00064B32' ;
+echo off
+@pause
+x > 109 = 1D
+k.weedyv
+color 04
+echo on
+/FOOD '1D'
+ /
